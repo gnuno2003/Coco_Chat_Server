60dfa3b4f6b612f093ab306307ff0e6c7d7c85b7
unknown <a19300101@ceti.mx>
2023-05-05 16:54:25 -0600

    Mensajes

--- src/db_conection_package/Mensaje_Usuario.java
+++ src/db_conection_package/Mensaje_Usuario.java
@@ -5,28 +5,30 @@ import java.util.Date;
  *
  * @author gnuno
  */
-public class MensajeUsuario {
+public class Mensaje_Usuario {
     public int id_mensaje_usuario;
     public int  remitente_usuario;
     public int  destinatario_usuario;
-    public Date fecha_mensaje;
+    public Date fecha_mensaje_usuario;
     public String mensaje_usuario;
     
-    public MensajeUsuario()
+    public Mensaje_Usuario()
     {
         id_mensaje_usuario = 0;
         remitente_usuario = 0;
         destinatario_usuario = 0;
-        fecha_mensaje = new Date();
+        fecha_mensaje_usuario = new Date();
         mensaje_usuario = "";
     }
     
-    public MensajeUsuario(int remitente_usuario, int destinatario_usuario, Date fecha_mensaje, String mensaje_usuario)
+    public Mensaje_Usuario(int remitente_usuario, int destinatario_usuario, Date fecha_mensaje_usuario, String mensaje_usuario)
     {
+        //autoincrementar o lo que le suponga
         this.id_mensaje_usuario = id_mensaje_usuario;
+        //
         this.remitente_usuario = remitente_usuario;
         this.destinatario_usuario = destinatario_usuario;
-        this.fecha_mensaje = fecha_mensaje;
+        this.fecha_mensaje_usuario = fecha_mensaje_usuario;
         this.mensaje_usuario = mensaje_usuario;
     }
 }
